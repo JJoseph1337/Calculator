36fa6b80f37f98cc388d91bec2b15a338d078f8c
JJoseph1337 <dassdass733@gmail.com>
2023-11-28 15:33:17 +0500

    Added operation icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
 
 function App() {
-	return <>
-  <p>Prvet</p></>;
+	return (
+		<>
+    
+		</>
+	);
 }
 
 export default App;
---
+++ src/ui/icons/EqualIcon.tsx
@@ -0,0 +1,18 @@
+const EqualIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="17"
+			height="11"
+			viewBox="0 0 17 11"
+			fill="none"
+		>
+			<path
+				d="M0.283142 0.965424H16.2831V4.24142H0.283142V0.965424ZM0.283142 7.58942H16.2831V10.8294H0.283142V7.58942Z"
+				fill="#F0F0F3"
+			/>
+		</svg>
+	);
+};
+
+export default EqualIcon;
